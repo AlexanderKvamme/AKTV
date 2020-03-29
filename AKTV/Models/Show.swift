144e6eb8cac77926089d790ab63da7d2203b782a
Alexander Kvamme <alexanderkvamme@gmail.com>
2020-03-29 16:05:31 +0200

    mapping tv shows working

--- AKTV/Models/Show.swift
+++ AKTV/Models/Show.swift
@@ -9,19 +9,19 @@
 import Foundation
 
 struct Show: Decodable {
-    var backdropPath: String
-    var firstAirDate: Date
-    var genreIds: [Int]
-    var id: Int
-    var name: String
-    var originCountry: [String]
-    var originalLanguage: String
-    var originalName: String
-    var overview: String
-    var popularity: String
-    var posterPath: String
-//    var voteAverage: String
-//    var voteCount: String
+    var backdropPath: String?
+    var firstAirDate: String?
+    var genreIds: [Int]?
+    var id: Int?
+    var name: String?
+    var originCountry: [String]?
+    var originalLanguage: String?
+    var originalName: String?
+    var overview: String?
+    var popularity: Double?
+    var posterPath: String?
+    var voteAverage: Int?
+    var voteCount: Int?
     
     enum CodingKeys: String, CodingKey {
         case id = "id"
@@ -33,10 +33,7 @@ struct Show: Decodable {
         case firstAirDate = "first_air_date"
         case genreIds = "genre_ids"
         case overview = "overview"
-        case popularity = "popularity"
+        case popularity
         case posterPath = "poster_path"
-        
-    //    case lastName = "user_last_name"
-    //    case age
     }
 }
