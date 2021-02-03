91fb14f4905b94a6618577db33922071bbae4211
Alexander Kvamme <alexanderkvamme@gmail.com>
2021-02-03 15:41:57 +0100

    Add DayModel

--- AKTV/AKDateFormatter.swift
+++ AKTV/AKDateFormatter.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 
+struct DayModel: Hashable, Comparable {
+    static func < (lhs: DayModel, rhs: DayModel) -> Bool {
+        return Int("\(lhs.year)\(lhs.month)\(lhs.day)")! < Int("\(rhs.year)\(rhs.month)\(rhs.day)")!
+    }
+
+    var day: Int
+    var month: Int
+    var year: Int
+
+}
+
 final class AKDateFormatter: NSObject {
     
     // MARK: Properties
