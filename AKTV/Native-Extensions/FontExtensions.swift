32a0b097f1c0b8f4670035c59260089930b877dc
Alexander Kvamme <alexanderkvamme@gmail.com>
2021-02-05 16:54:53 +0100

    Add fonts

---
+++ AKTV/Native-Extensions/FontExtensions.swift
@@ -0,0 +1,47 @@
+//
+//  FontExtensions.swift
+//  AKTV
+//
+//  Created by Alexander Kvamme on 05/02/2021.
+//  Copyright © 2021 Alexander Kvamme. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+enum DINWeights: String {
+    case light  = "DINRoundPro-Light"
+    case medium = "DINRoundPro-Medium"
+    case bold   = "DINRoundPro-Bold"
+    case black  = "DINRoundPro-Black"
+}
+
+enum GilroyWeights: String {
+    case light      = "Gilroy-Thin"
+    case regular    = "Gilroy-Regular"
+    case medium     = "Gilroy-Medium"
+    case semibold   = "Gilroy-Semibold"
+    case bold       = "Gilroy-Bold"
+    case extraBold  = "Gilroy-Extrabold"
+    case heavy      = "Gilroy-Heavy"
+}
+
+extension UIFont {
+    static func round(_ weight: DINWeights, _ size: CGFloat) -> UIFont {
+        UIFont(name: weight.rawValue, size: size)!
+    }
+
+    static func gilroy(_ weight: GilroyWeights, _ size: CGFloat) -> UIFont {
+        UIFont(name: weight.rawValue, size: size)!
+    }
+}
+
+extension Font {
+    static func round(_ weight: DINWeights, _ size: CGFloat) -> Font {
+        Font.custom(weight.rawValue, size: size)
+    }
+
+    static func gilroy(_ weight: GilroyWeights, _ size: CGFloat) -> Font {
+        Font.custom(weight.rawValue, size: size)
+    }
+}
