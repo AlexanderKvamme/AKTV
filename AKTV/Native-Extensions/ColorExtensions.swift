e85ab4ee6587aefdd7fd379aff9650cfa8066ce9
Alexander Kvamme <alexanderkvamme@gmail.com>
2021-02-05 17:51:27 +0100

    Add Colors and color conveniences

---
+++ AKTV/Native-Extensions/ColorExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  ColorExtensions.swift
+//  AKTV
+//
+//  Created by Alexander Kvamme on 05/02/2021.
+//  Copyright © 2021 Alexander Kvamme. All rights reserved.
+//
+
+import Foundation
---
+++ AKTV/Themes.swift
@@ -0,0 +1,14 @@
+//
+//  Themes.swift
+//  AKTV
+//
+//  Created by Alexander Kvamme on 05/02/2021.
+//  Copyright © 2021 Alexander Kvamme. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+let light = Color.init(hex: "#F6F6FB")
+let dark = Color.init(hex: "#3A3A3C")
+
