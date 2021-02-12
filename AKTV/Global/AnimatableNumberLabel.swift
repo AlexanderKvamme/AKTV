fb9fc74b14b63ed9ce294ce927fc1d65a8bf3dc4
Alexander Kvamme <alexanderkvamme@gmail.com>
2021-02-13 00:07:40 +0100

    Add AnimatableNumberLabel

---
+++ AKTV/Global/AnimatableNumberLabel.swift
@@ -0,0 +1,30 @@
+//
+//  AnimatableNumberLabel.swift
+//  AKTV
+//
+//  Created by Alexander Kvamme on 12/02/2021.
+//  Copyright © 2021 Alexander Kvamme. All rights reserved.
+//
+
+import UIKit
+
+
+final class UpCountingLabel: UILabel {
+
+    // MARK: - Initializers
+
+    init() {
+        super.init(frame: .zero)
+
+        adjustsFontSizeToFitWidth = true
+        font = UIFont.gilroy(.bold, 28)
+        textColor = UIColor(light)
+        textAlignment = .center
+        text = "0"
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
