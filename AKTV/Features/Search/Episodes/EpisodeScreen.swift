074f97e40ef974d2df0e328a9bdb3c01f9a44683
Alexander Kvamme <alexanderkvamme@gmail.com>
2021-03-12 21:50:22 +0100

    Add app icon

---
+++ AKTV/Features/Search/Episodes/EpisodeScreen.swift
@@ -0,0 +1,9 @@
+//
+//  EpisodeScreen.swift
+//  AKTV
+//
+//  Created by Alexander Kvamme on 24/02/2021.
+//  Copyright © 2021 Alexander Kvamme. All rights reserved.
+//
+
+import Foundation
---
+++ AKTV/Features/UpcomingScreen/Animations/UpcomingToDetailedAnimationController.swift
@@ -0,0 +1,9 @@
+//
+//  UpcomingToDetailedAnimationController.swift
+//  AKTV
+//
+//  Created by Alexander Kvamme on 23/02/2021.
+//  Copyright © 2021 Alexander Kvamme. All rights reserved.
+//
+
+import Foundation
