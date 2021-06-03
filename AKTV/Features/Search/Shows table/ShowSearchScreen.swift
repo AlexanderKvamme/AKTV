ad4412d719264688676320b9920491909659efc9
Alexander Kvamme <alexanderkvamme@gmail.com>
2021-06-04 00:16:31 +0200

    Make add search screen white

--- AKTV/Features/Search/Shows table/ShowSearchScreen.swift
+++ AKTV/Features/Search/Shows table/ShowSearchScreen.swift
@@ -1,21 +1,9 @@
-//
-//  ShowSearchScreen.swift
-//  AKTV
-//
-//  Created by Alexander Kvamme on 25/03/2020.
-//  Copyright © 2020 Alexander Kvamme. All rights reserved.
-//
-
-import Foundation
 import UIKit
 
-//
 
 protocol SeasonPresenter {
     func displaySeason(showId: Int?, seasonNumber: Int?)
 }
-
-// This is where the app starts
 
 protocol ModelPresenter {
     func displayShow(_ id: Int?)
@@ -23,15 +11,58 @@ protocol ModelPresenter {
 }
 
 
-final class ShowsSearchScreen: UIViewController {
+private final class HeaderContainer: UIViewController {
+
+    private let headerField = UILabel()
+    let searchField = UITextField()
+
+    override func viewDidLoad() {
+        setup()
+        addSubviewsAndConstraints()
+    }
+
+    func setup() {
+        headerField.font = UIFont.gilroy(.bold, 20)
+        headerField.textAlignment = .center
+        headerField.text = "What are you looking for?"
+        headerField.textColor = UIColor(dark)
+
+        searchField.textColor = UIColor(light)
+        searchField.backgroundColor = .clear
+        searchField.textAlignment = .center
+        searchField.placeholder = "Game of thrones"
+        searchField.font = UIFont.gilroy(.heavy, 60)
+        searchField.textColor = UIColor(dark)
+        searchField.isUserInteractionEnabled = true
+        searchField.becomeFirstResponder()
+        searchField.adjustsFontSizeToFitWidth = true
+    }
+
+    func addSubviewsAndConstraints() {
+        view.addSubview(headerField)
+        headerField.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()//.offset(40)
+            make.left.right.bottom.equalToSuperview()
+        }
+
+        view.addSubview(searchField)
+        searchField.snp.makeConstraints { (make) in
+            make.top.equalTo(headerField).offset(80)
+            make.left.right.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+
+
+final class ShowsSearchScreen: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // MARK: Properties
 
-    private let headerField = UILabel()
-    private let searchField = UITextField()
     private let episodesSearchResultViewController = UITableViewController()
-    private let episodesSearchResultDataDelegate = ShowsDataDelegate()
+    private let episodesSearchResultDataDelegate = self
     private let apiDao: APIDAO
+    private let headerContainer = HeaderContainer()
     
     // MARK: Initializers
     
@@ -49,55 +80,72 @@ final class ShowsSearchScreen: UIViewController {
     // MARK: Private methods
     
     private func setup() {
-        view.backgroundColor = UIColor(dark)
+        view.backgroundColor = UIColor(hex: "#F6FAFB")
 
-        headerField.font = UIFont.gilroy(.semibold, 20)
-        headerField.textAlignment = .center
-        headerField.textColor = UIColor(light)
-        headerField.text = "What are you looking for?"
+        headerContainer.searchField.delegate = self
 
-        searchField.backgroundColor = UIColor(dark)
-        searchField.textColor = UIColor(light)
-        searchField.textAlignment = .center
-        searchField.placeholder = "Game of thrones"
-        searchField.font = UIFont.gilroy(.heavy, 60)
-        searchField.delegate = self
-        searchField.isUserInteractionEnabled = true
-        searchField.becomeFirstResponder()
-        searchField.adjustsFontSizeToFitWidth = true
-
-        episodesSearchResultViewController.tableView.dataSource = episodesSearchResultDataDelegate
-        episodesSearchResultViewController.tableView.delegate = episodesSearchResultDataDelegate
+        episodesSearchResultViewController.tableView.dataSource = self
+        episodesSearchResultViewController.tableView.delegate = self
         episodesSearchResultViewController.tableView.estimatedRowHeight = ShowCell.estimatedHeight
         episodesSearchResultViewController.tableView.backgroundColor = .clear
+        episodesSearchResultViewController.tableView.separatorStyle = .none
 
-        episodesSearchResultDataDelegate.detailedShowPresenter = self
+        detailedShowPresenter = self
     }
     
     private func addSubviewsAndConstraints() {
-        view.addSubview(searchField)
-        searchField.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(80)
-            make.left.right.equalToSuperview().inset(16)
-            make.height.equalTo(200)
-        }
+        view.addSubview(headerContainer.view)
 
-        view.addSubview(headerField)
-        headerField.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(100)
-            make.left.right.equalToSuperview()
-        }
-        
-        let tv = episodesSearchResultViewController
-        view.addSubview(tv.view)
+        headerContainer.view.frame = CGRect(x: 0, y: 0, width: screenWidth, height: 240)
+        view.addSubview(episodesSearchResultViewController.view)
         addChild(episodesSearchResultViewController)
         
         episodesSearchResultViewController.view.snp.makeConstraints { (make) in
-            make.top.equalTo(searchField.snp.bottom)
+            make.top.equalTo(headerContainer.view.snp.bottom)
             make.left.right.bottom.equalToSuperview()
         }
     }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset
+        if (offset.y > 200) {
+            return
+        }
+
+        headerContainer.view.frame.origin.y = -offset.y
+    }
+
+    // MARK: Properties
+
+    var shows = [Show]()
+    var detailedShowPresenter: ModelPresenter?
+
+    // MARK: Delegate methods
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shows.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let show = shows[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: ShowCell.identifier) ?? ShowCell(for: shows[indexPath.row])
+        cell.backgroundColor = .clear
+        if let cell = cell as? ShowCell {
+            cell.update(with: show)
+        } else {
+            fatalError("could not cast to ShowCell")
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        detailedShowPresenter?.displayShow(shows[indexPath.row].id)
+    }
 }
+
+
+
+
 // MARK: - DetailedShowPresenter conformance
 
 extension ShowsSearchScreen: ModelPresenter {
@@ -133,7 +181,7 @@ extension ShowsSearchScreen: UITextFieldDelegate {
         
         _ = apiDao.searchShows(string: searchterm) { (shows) in
             DispatchQueue.main.async {
-                self.episodesSearchResultDataDelegate.shows = shows
+                self.shows = shows
                 self.episodesSearchResultViewController.tableView.reloadData()
             }
         }
--- AKTV/Features/Search/Shows table/ShowsDataDelegate.swift
+++ AKTV/Features/Search/Shows table/ShowsDataDelegate.swift
@@ -9,36 +9,6 @@
 import Foundation
 import UIKit
 
-final class ShowsDataDelegate: NSObject, UITableViewDataSource, UITableViewDelegate {
-    
-    // MARK: Properties
-    
-    var shows = [Show]()
-    var detailedShowPresenter: ModelPresenter?
-    
-    // MARK: Delegate methods
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shows.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let show = shows[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: ShowCell.identifier) ?? ShowCell(for: shows[indexPath.row])
-        cell.backgroundColor = .clear
-        if let cell = cell as? ShowCell {
-            cell.update(with: show)
-        } else {
-            fatalError("could not cast to ShowCell")
-        }
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        detailedShowPresenter?.displayShow(shows[indexPath.row].id)
-    }
-}
-
 final class ShowCell: UITableViewCell {
     
     // MARK: Properties
@@ -67,7 +37,7 @@ final class ShowCell: UITableViewCell {
         selectionStyle = .none
 
         header.text = "Shazam"
-        header.textColor = UIColor(light)
+        header.textColor = UIColor(dark)
         header.font = UIFont.gilroy(.bold, 20)
         header.layoutMargins = UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 0)
         header.backgroundColor = .clear
