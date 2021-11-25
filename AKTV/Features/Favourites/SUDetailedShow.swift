72bf20522f46052bbde8b6c8b8026ef3c0f6ad4a
Alexander Kvamme <alexanderkvamme@gmail.com>
2021-11-25 16:04:00 +0100

    Fix indentation

--- AKTV/Features/Favourites/SUDetailedShow.swift
+++ AKTV/Features/Favourites/SUDetailedShow.swift
@@ -10,48 +10,48 @@ import SwiftUI
 import Kingfisher
 
 struct SUDetailedEntity: View {
-
+    
     @State var entity: Entity
-
+    
     var body: some View {
         ZStack {
             Rectangle()
                 .foregroundColor(light)
                 .ignoresSafeArea()
-        VStack(spacing: 24) {
-            KFImage(entity.getMainGraphicsURL())
-                .fade(duration: 0.25)
-                .frame(width: screenWidth-32, height: 300, alignment: .top)
-                .scaledToFit()
-                .cornerRadius(14)
-                .clipped()
-                .shadow(radius: 5)
-
-            Text(entity.name)
-                .font(Font.gilroy(GilroyWeights.bold, 32))
-                .foregroundColor(dark)
-
-            Text("Add some more stuff here")
-                .font(Font.gilroy(.regular, 16))
-                .foregroundColor(dark)
-
-            Spacer()
+            VStack(spacing: 24) {
+                KFImage(entity.getMainGraphicsURL())
+                    .fade(duration: 0.25)
+                    .frame(width: screenWidth-32, height: 300, alignment: .top)
+                    .scaledToFit()
+                    .cornerRadius(14)
+                    .clipped()
+                    .shadow(radius: 5)
+                
+                Text(entity.name)
+                    .font(Font.gilroy(GilroyWeights.bold, 32))
+                    .foregroundColor(dark)
+                
+                Text("Add some more stuff here")
+                    .font(Font.gilroy(.regular, 16))
+                    .foregroundColor(dark)
+                
+                Spacer()
+            }
         }
     }
-}
-
-struct EntityImage: View {
-
-    @State var show: Show
-
-    var body: some View {
-        let backdropPath = show.backdropPath!
-        let url = URL(string: APIDAO.imdbImageRoot+backdropPath)!
-
-        VStack {
-            AsyncImage(url: url)
+    
+    struct EntityImage: View {
+        
+        @State var show: Show
+        
+        var body: some View {
+            let backdropPath = show.backdropPath!
+            let url = URL(string: APIDAO.imdbImageRoot+backdropPath)!
+            
+            VStack {
+                AsyncImage(url: url)
+            }
         }
     }
-}
 }
 
