458e87e7ea127b8e34a166c0888826ebe2557062
Alexander Kvamme <alexanderkvamme@gmail.com>
2022-01-13 11:28:23 +0100

    Add uikit fav list with diffable datasource

--- AKTV/Features/Favourites/UIKitFavouriteScreen.swift
+++ AKTV/Features/Favourites/UIKitFavouriteScreen.swift
@@ -10,22 +10,20 @@ final class UIKitFavouriteScreen<T: Entity>: UIViewController where T: Hashable 
     
     enum Section: Int, CaseIterable {
         case favourites
+        case all
     }
     
     typealias Product = T
     typealias ProductList = [Product]
     typealias Cell = TestCell
     
-    private var collectionView: UICollectionView!
+    private lazy var collectionView = makeCollectionView()
     private lazy var dataSource = makeDataSource()
     
     // MARK: - Initializers
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nil, bundle: nil)
-        
-        setup()
-        addSubviewsAndConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -34,34 +32,33 @@ final class UIKitFavouriteScreen<T: Entity>: UIViewController where T: Hashable 
     
     // MARK: - Life cycle
     
-    override func viewDidDisappear(_ animated: Bool) {
+    override func viewDidLoad() {
+        setup()
+        addSubviewsAndConstraints()
+        
+        collectionView.register(TestCell.self, forCellWithReuseIdentifier: diffableCellReuseID)
+        collectionView.dataSource = dataSource
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-//        productListDidLoad(Show.mocks)
+        if let test = Show.mocks as? [T] {
+            print("casted: ", test.count)
+            productListDidLoad(test)
+        }
     }
     
     // MARK: - Methods
     
-    private func setup() {
-        collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
-        collectionView.backgroundColor = .green
-        view.backgroundColor = .orange
-        
-        let layoutConfig = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
-        _ = UICollectionViewCompositionalLayout.list(using: layoutConfig)
-        
-        // Create cell registration that defines how data should be shown in a cell
-        _ = UICollectionView.CellRegistration<UICollectionViewListCell, MediaPickable> { (cell, indexPath, item) in
-            
-            // Define how data should be shown using content configuration
-            let content = cell.defaultContentConfiguration()
-//            content.image = item.image
-//            content.text = item.name
-            
-            // Assign content configuration to cell
-            cell.contentConfiguration = content
-        }
+    func makeCollectionView() -> UICollectionView {
+        return UICollectionView(
+            frame: .zero,
+            collectionViewLayout: makeCollectionViewLayout()
+        )
     }
+    
+    private func setup() {}
     
     private func addSubviewsAndConstraints() {
         view.addSubview(collectionView)
@@ -86,52 +83,42 @@ final class UIKitFavouriteScreen<T: Entity>: UIViewController where T: Hashable 
         dataSource.apply(snapshot)
     }
     
-    typealias CellRegistration = UICollectionView.CellRegistration<Cell, Product> // string should be the data type
+    typealias CellRegistration = UICollectionView.CellRegistration<Cell, Product>
     
-    // let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: Self.cellReuseID, for: indexPath) as! TestCell
     func makeCellRegistration() -> CellRegistration {
         CellRegistration { cell, indexPath, product in
-            print("bam making cell for ", indexPath)
             var config = cell.defaultContentConfiguration()
-            config.text = "bam"
+            config.text = product.name
             cell.contentConfiguration = config
-            cell.accessories = [.disclosureIndicator()]
+            cell.frame = TestCell.size
         }
     }
-    
-    func makeGridLayoutSection() -> NSCollectionLayoutSection {
-        // Each item will take up half of the width of the group
-        // that contains it, as well as the entire available height:
-        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(0.5),
-            heightDimension: .fractionalHeight(1)
-        ))
-        
-        // Each group will then take up the entire available
-        // width, and set its height to half of that width, to
-        // make each item square-shaped:
-        let group = NSCollectionLayoutGroup.horizontal(
-            layoutSize: NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1),
-                heightDimension: .fractionalWidth(0.5)
-            ),
-            subitem: item,
-            count: 2
-        )
-        
-        return NSCollectionLayoutSection(group: group)
+}
+
+private extension UIKitFavouriteScreen {
+    func makeCollectionViewLayout() -> UICollectionViewLayout {
+        UICollectionViewCompositionalLayout {
+            [weak self] sectionIndex, _ in
+            
+            switch Section(rawValue: sectionIndex) {
+            case .favourites:
+                return self?.makeListLayoutSection()
+            case .all:
+                return self?.makeGridLayoutSection()
+            case nil:
+                return nil
+            }
+        }
     }
-    
+}
+
+private extension UIKitFavouriteScreen {
     func makeListLayoutSection() -> NSCollectionLayoutSection {
-        // Here, each item completely fills its parent group:
         let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(1),
             heightDimension: .fractionalHeight(1)
         ))
         
-        // Each group then contains just a single item, and fills
-        // the entire available width, while defining a fixed
-        // height of 50 points:
         let group = NSCollectionLayoutGroup.vertical(
             layoutSize: NSCollectionLayoutSize(
                 widthDimension: .fractionalWidth(1),
@@ -142,52 +129,36 @@ final class UIKitFavouriteScreen<T: Entity>: UIViewController where T: Hashable 
         
         return NSCollectionLayoutSection(group: group)
     }
-    
-    func makeCollectionViewLayout() -> UICollectionViewLayout {
-        UICollectionViewCompositionalLayout {
-            [weak self] sectionIndex, environment in
-            
-            switch Section(rawValue: sectionIndex) {
-//            case .featured, .onSale:
-//                return self?.makeGridLayoutSection()
-            case .favourites:
-                // Creating our table view-like list layout using
-                // a given appearence. Here we simply use 'plain':
-                return .list(
-                    using: UICollectionLayoutListConfiguration(
-                        appearance: .plain
-                    ),
-                    layoutEnvironment: environment
-                )
-            case nil:
-                return nil
-            }
-        }
-    }
-    
-    func makeCollectionView() -> UICollectionView {
-        let layout = UICollectionViewCompositionalLayout.list(
-            using: UICollectionLayoutListConfiguration(
-                appearance: .insetGrouped
-            )
-        )
-        
-        return UICollectionView(
-            frame: .zero,
-            collectionViewLayout: layout
-        )
-    }
 }
-
 
 extension UICollectionView.CellRegistration {
     var cellProvider: (UICollectionView, IndexPath, Item) -> Cell {
         return { collectionView, indexPath, product in
-            collectionView.dequeueConfiguredReusableCell(
+            return collectionView.dequeueConfiguredReusableCell(
                 using: self,
                 for: indexPath,
                 item: product
             )
         }
+    }
+}
+
+private extension UIKitFavouriteScreen {
+    func makeGridLayoutSection() -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        ))
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .fractionalWidth(0.5)
+            ),
+            subitem: item,
+            count: 2
+        )
+        
+        return NSCollectionLayoutSection(group: group)
     }
 }
--- TestCell.swift
+++ TestCell.swift
@@ -5,7 +5,19 @@ final class TestCell: UICollectionViewListCell {
     
     // MARK: - Properties
     
+    static let size = CGRect(x: 0, y: 0, width: screenWidth, height: 64)
+    
     // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: Self.size)
+        backgroundColor = .red
+        contentView.backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Life cycle
     
