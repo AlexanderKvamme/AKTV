883e190927afb88013b7fcb671dab89ef1339ead
Alexander Kvamme <alexanderkvamme@gmail.com>
2022-07-27 12:50:48 +0200

    Add README

---
+++ AKTV/Features/PushNotifications/PushNotificationManager.swift
@@ -0,0 +1,9 @@
+//
+//  PushNotificationManager.swift
+//  AKTV
+//
+//  Created by Alexander Kvamme on 26/07/2022.
+//  Copyright © 2022 Alexander Kvamme. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,2 @@
+# To send push notification when running a simulator
+xcrun simctl push booted com.HomeOfficesInternational.AKTV first.apn
